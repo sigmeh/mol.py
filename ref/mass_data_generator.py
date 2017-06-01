5293f848e1928b735fb22f289dbdc10addfdcf60
Mark Harvey <markedwinharvey@gmail.com>
2017-06-01 22:46:23 +0200

    change attribute "abundances" to "intensities" and modify docstring

--- ref/mass_data_generator.py
+++ ref/mass_data_generator.py
@@ -1,15 +1,28 @@
 #!/usr/bin/env python
 '''
 Parse mass data from mass_data.txt into element objects 
+ *changed abundance/abundances to intensity/intensities for consistency across modules (when building fragments
+
+>>> elements['Os'].atomic_num
+76
+
+>>> elements['Ru'].masses
+[95.907597681, 97.905287111, 98.905939307, 99.904219664, 100.905582219, 101.904349503, 103.905430145]
+
+>>> elements['Si'].intensities
+[0.9223, 0.0467, 0.031]
+
 '''
 import numpy as np
+import sys
+
 elements = {}
 
 class element(object):
-	def __init__(self,atomic_num,mass,abundance):
+	def __init__(self, atomic_num, mass, intensity):
 		self.atomic_num = atomic_num
 		self.masses = [mass]
-		self.abundances = [abundance]
+		self.intensities = [intensity]
 	
 
 def generate_mass_data():
@@ -20,25 +33,27 @@ def generate_mass_data():
 	els = {}
 	
 	for line in mass_data:
+
 		i = line.split(' ')
 		atomic_num = int(i[0])
 		el = i[2]
 		mass = float(i[3].strip('#'))
-		abundance = float(i[5])/100.	
+		intensity = float(i[5])/100.	
 		
 		if el not in elements:
-			elements[el] = element(atomic_num=atomic_num,mass=mass,abundance=abundance)
+			elements[el] = element(atomic_num=atomic_num, mass=mass, intensity=intensity)
 		else:
 			elements[el].masses.append(mass)
-			elements[el].abundances.append(abundance)
+			elements[el].intensities.append(intensity)
 
 	for el in elements:
 		if len(elements[el].masses) == 1:
 			elements[el].molar_mass = elements[el].masses[0]
-		elif all([x == 0. for x in elements[el].abundances]):
+		elif all([x == 0. for x in elements[el].intensities]):
 			elements[el].molar_mass = sum(elements[el].masses)/len(elements[el].masses)
 		else:
-			elements[el].molar_mass = sum(np.array(elements[el].masses) * np.array(elements[el].abundances))
+			elements[el].molar_mass = sum(np.array(elements[el].masses) * np.array(elements[el].intensities))
+	
 	return elements
 
 
